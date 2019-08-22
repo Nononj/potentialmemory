e39bfe01937f284ef64201db853656d0f7cfc446
Nononj <54393166+Nononj@users.noreply.github.com>
2019-08-22 13:01:07 +0300

    Add files via upload

---
+++ t.vhd
@@ -0,0 +1,63 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std;
+use work.FloatPt.all;
+
+entity mppt2 is port(
+	vpv : in numeric_std.signed(15 downto 0);
+	ipv : in numeric_std.signed(15 downto 0);
+	clock : in std_logic;
+	reset : in std_logic;
+	
+	done : out std_logic;
+	ref : out numeric_std.signed(15 downto 0));
+end mppt2;
+
+architecture Behavioral of mppt2 is
+--	signal pvp : std_logic_vector(15 downto 0);
+	signal vpv_pt : std_logic_vector( 31 downto 0);
+	signal ipv_pt : std_logic_vector(31 downto 0);
+	signal res_32 : std_logic_vector(31 downto 0);
+	signal go_mul: std_logic;
+	signal refa : std_logic_vector(31 downto 0);
+	
+begin
+	
+	MUL1 : FPP_MULT port map
+	(	
+		A => vpv_pt, 
+		B => ipv_pt,
+		clk => clock, 
+		reset => reset,
+		go => go_mul,
+		done => done,
+		overflow => open, 
+		result => res_32
+	);
+	
+	process
+	begin
+		wait for 20 ns;
+		vpv_pt <= SIGNED_TO_FP(vpv);
+		ipv_pt <= SIGNED_TO_FP(ipv);
+		wait for 22 ns;
+		go_mul <= '1';
+	end process;
+	
+	-- process(res_32)
+	-- begin
+		-- ref <= FP_TO_SIGNED(res_32, 16);
+	-- end process;
+	
+	process(res_32)
+	begin
+		refa <= res_32;
+		vpv_pt <= SIGNED_TO_FP(vpv'delayed(100 ns));
+		ipv_pt <= SIGNED_TO_FP(ipv'delayed(100 ns));
+		go_mul <= '1';
+	end process;
+	
+	--if ref < ref'delayed(100 ns);
+end Behavioral;
+
+	
---
+++ tmpp2.vhd
@@ -0,0 +1,60 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_std;
+
+entity tmppt2 is
+end tmppt2;
+
+
+
+architecture Behavioral of tmppt2 is
+
+	component mppt2 is port(
+		vpv : in numeric_std.signed(15 downto 0);
+		ipv : in numeric_std.signed(15 downto 0);
+		clock : in std_logic;
+		reset : in std_logic;
+	
+		done : out std_logic;
+		ref : out numeric_std.signed(15 downto 0));
+	end component;
+
+	signal tclock : std_logic;
+	signal tvpv : numeric_std.signed(15 downto 0);
+	signal tipv : numeric_std.signed(15 downto 0);
+	signal tref :   numeric_std.signed(15 downto 0);
+	signal treset : std_logic;
+	signal tdone : std_logic;
+	
+	begin
+	inst : mppt2 port map (
+		clock => tclock,
+		vpv => tvpv,
+		ipv => tipv,
+		ref => tref,
+		reset => treset,
+		done => tdone 
+		);
+
+	process
+  	begin
+  		tclock <= '0';
+  		wait for 5 ns;
+  		tclock <= '1';
+  		wait for 5 ns;
+ 	end process;
+	
+	process
+  	begin
+		wait for 10 ns;
+			tvpv <="0000000000000011" ;
+			tipv <="0000000000011111" ;
+		wait for 60 ns;
+			tvpv <="0000000000000001" ;
+			tipv <="0000000000000001" ;
+		wait for 10 ns;
+	end process;
+	
+end Behavioral;
+	
+	
